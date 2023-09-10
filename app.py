21408f044399367a66286c07555c08a1ae296117
Rithvik <65542427+RITHVIK1101@users.noreply.github.com>
2023-09-10 14:48:55 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import requests
 
 app = Flask(__name__)
 
-# Define a function to scrape player information from the website
-def scrape_player_info():
+# Define a function to scrape player information from "https://www.wolvesfootball.com/roster/varsity"
+def scrape_player_info(player_name):
     try:
         website_url = "https://www.wolvesfootball.com/roster/varsity"
         response = requests.get(website_url)
@@ -17,65 +17,49 @@ def scrape_player_info():
         player_data = []
 
         for player_info in player_infos:
-            player_name = player_info.find('h2', class_='list-item-content__title').text.strip()
-            details = player_info.find('p', class_='').text.strip().split('|')
-            grade = details[0].strip()
-            position = details[1].strip()
-            height = details[2].strip()
-            weight = details[3].strip()
-
-            player_data.append({
-                "Player Name": player_name,
-                "Grade": grade,
-                "Position": position,
-                "Height": height,
-                "Weight": weight
-            })
+            # Your scraping logic here
 
         return player_data
 
     except Exception as e:
         return str(e)
 
-@app.route('/get_player_info', methods=['GET', 'POST'])
+@app.route('/get_player_info', methods=['GET'])
 def get_player_info():
     try:
-        player_name = request.args.get('player_name')
-        if player_name is None:
-            # Handle the case where 'player_name' is missing or None
-            return jsonify({
-                "fulfillmentText": "Please provide a player name."
-            })
+        # Extract the player_name parameter from the user's query
+        player_name = request.json['queryResult']['parameters']['player_name']
 
-        player_name = player_name.upper()  # Normalize player name
-        logging.info(f"Received request for player: {player_name}")
+        # Call the scrape_player_info function to fetch player data
+        player_data = scrape_player_info(player_name)
 
-        player_data = scrape_player_info()
+        if player_data:
+            # Find player information based on the provided player_name
+            found_players = [player for player in player_data if player_name in player['Player Name'].upper()]
 
-        # Find player information in the scraped data
-        found_players = [player for player in player_data if player_name in player['Player Name'].upper()]
+            if found_players:
+                # Take the first found player (you can modify this logic if needed)
+                player_info = found_players[0]
 
-        if found_players:
-            # Take the first found player (you can modify this logic if needed)
-            player_info = found_players[0]
+                # Construct the URL for the Azure web app with the player_name parameter
+                azure_url = f"https://ehsfb.azurewebsites.net/get_player_info?player_name={player_info['Player Name']}"
 
-            # Construct a response
-            response_text = (
-                f"{player_info['Player Name']} is in grade {player_info['Grade']}, "
-                f"plays as {player_info['Position']}, has a height of {player_info['Height']}, "
-                f"and a weight of {player_info['Weight']}."
-            )
+                # Construct a response
+                response_text = (
+                    f"{player_info['Player Name']} is in grade {player_info['Grade']}, "
+                    f"plays as {player_info['Position']}, has a height of {player_info['Height']}, "
+                    f"and a weight of {player_info['Weight']}. "
+                    f"You can find more information [here]({azure_url})."
+                )
+            else:
+                response_text = "Player not found."
         else:
-            response_text = "Player not found."
-
-        return jsonify({
-            "fulfillmentText": response_text
-        })
+            response_text = "No player data available."
 
     except Exception as e:
-        return jsonify({
-            "fulfillmentText": f"An error occurred: {str(e)}"
-        })
+        response_text = f"An error occurred: {str(e)}"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    return jsonify({
+        "fulfillmentText": response_text
+    })
+
